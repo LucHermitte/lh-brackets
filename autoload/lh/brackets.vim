LucHermitte/lh-brackets

commit d4b2746cecfada0c1e5ce847f253c7ac01326ad6

BUG: Support older versions of vim
The following issues were found by Troy Custis Jr:

- `strwidth()` and `abs()` are not always available
- `function('autoload#function')` did not work before Vim 7.2-061

autoload/lh/brackets.vim
@@ -3,9 +3,10 @@
 " Author:       Luc Hermitte <EMAIL:hermitte {at} free {dot} fr>
 "               <URL:http://github.com/LucHermitte>
 " License:      GPLv3 with exceptions
-"               <URL:http://github.com/LucHermitte/lh-brackets/License.md>
-" Version:      3.0.0
+"               <URL:http://github.com/LucHermitte/lh-brackets/tree/master/License.md>
+" Version:      3.0.1
 " Created:      28th Feb 2008
+" Last Update:  01st Mar 2016
 "------------------------------------------------------------------------
 " Description:
 "               This autoload plugin defines the functions behind the command
@@ -23,6 +24,8 @@
 "
 "------------------------------------------------------------------------
 " History:
+" Version 3.0.1:
+"               * Support older versions of vim, thanks to Troy Curtis Jr
 " Version 3.0.0:
 "               * Support for closing all markers and jumping to the last one
 " Version 2.3.5:
@@ -410,10 +413,10 @@ function! lh#brackets#opener(trigger, escapable, nl, Open, Close, areSameTrigger
       " Now the mapping is a :map-<expr>, we cannot break the line with a
       " :setline(), we'll have to move the cursor
       if 0
-	call setline(line('.'), head)
-	" An undo break is added here. I'll have to investigate that someday
-	let before = "\<cr>".substitute(before, '^\s\+', '', '')
-	return lh#map#insert_seq(a:trigger, before.open.'!cursorhere!'.close.'!mark!'.after)
+        call setline(line('.'), head)
+        " An undo break is added here. I'll have to investigate that someday
+        let before = "\<cr>".substitute(before, '^\s\+', '', '')
+        return lh#map#insert_seq(a:trigger, before.open.'!cursorhere!'.close.'!mark!'.after)
       endif
       let delta_to_line_cut = lh#encoding#strlen(before)
       let delta_to_insertion = lh#encoding#strlen(substitute(before, '^\s\+', '', ''))
@@ -462,10 +465,9 @@ function! s:JumpOverAllClose(chars, ...) abort
   let ll = getline('.')[p : ] " ignore char under cursor, look after
   let m = matchstr(ll, '\v^(['.a:chars.']|'.lh#marker#txt('.{-}').')+')
   " echomsg ll.'##'.m.'##'
-  let lm = strwidth(m)
   let len_match = lh#encoding#strlen(m)
   if lm
-    let del_mark = repeat("\<del>", lm)
+    let del_mark = repeat("\<del>", len_match)
     let del_mark .= substitute(m, '[^'.a:chars.']', '', 'g')
   endif
   " Is there an optional terminal mark to check and merge/add (like: «»;«») ?
@@ -473,7 +475,7 @@ function! s:JumpOverAllClose(chars, ...) abort
     let remaining = ll[len_match : ]
     " echomsg "rem: <<".remaining.">>"
     let match_rem = matchstr(remaining, '^\('.lh#marker#txt('.\{-}').'\)*'.a:1.'\('.lh#marker#txt('.\{-}').'\)*')
-    let len_match_rem = strwidth(match_rem)
+    let len_match_rem = lh#encoding#strlen(match_rem)
     if len_match_rem
       let del_mark = repeat("\<del>", len_match_rem).del_mark
     endif
@@ -506,7 +508,7 @@ function! s:Jump() abort
   let ll = getline('.')[p : ]
   " echomsg ll
   let m = matchstr(ll, '^'.lh#marker#txt('.\{-}'))
-  let lm = strwidth(m)
+  let lm = lh#encoding#strlen(m)
   let del_mark = repeat("\<del>", lm)
   return s:k_move_prefix."\<right>".del_mark
 endfunction

VimFlavor
@@ -1,2 +1,2 @@
-flavor 'LucHermitte/lh-vim-lib', '>= 3.3.10'
+flavor 'LucHermitte/lh-vim-lib', '>= 3.8.0'
 flavor 'LucHermitte/lh-dev', '>= 1.3.5'

autoload/lh/map.vim
@@ -4,16 +4,17 @@
 "		<URL:http://github.com/LucHermitte>
 " License:      GPLv3 with exceptions
 "               <URL:http://github.com/LucHermitte/lh-brackets/tree/master/License.md>
-" Version:      3.0.0
-let s:k_version = '300'
+" Version:      3.0.1
+let s:k_version = '301'
 " Created:      03rd Nov 2015
-" Last Update:  10th Dec 2015
+" Last Update:  01st Mar 2016
 "------------------------------------------------------------------------
 " Description:
 "       API plugin: Several mapping-oriented functions
 "
 "------------------------------------------------------------------------
 " History:
+"       v3.0.1 Support older versions of vim, thanks to Troy Curtis Jr
 "       v3.0.0 !mark! & co have been deprecated as mappings
 "       v2.3.0 functions moved from plugin/misc_map.vim
 " TODO:
@@ -513,7 +514,7 @@ endfunction
 " See vim patch 7.4.849
 function! lh#map#_move_cursor_on_the_current_line(offset) abort
   let move = a:offset > 0 ? "\<right>" : "\<left>"
-  return repeat(s:k_move_prefix.move, abs(a:offset))
+  return repeat(s:k_move_prefix.move, lh#math#abs(a:offset))
 endfunction
 
 "------------------------------------------------------------------------

plugin/common_brackets.vim
@@ -4,8 +4,8 @@
 "               <URL:http://github.com/LucHermitte/lh-brackets>
 " License:      GPLv3 with exceptions
 "               <URL:http://github.com/LucHermitte/lh-brackets/tree/master/License.md>
-" Version:      3.0.0
-let s:version = 300
+" Version:      3.0.1
+let s:version = 301
 " Purpose:      {{{1
 "               This file defines a command (:Brackets) that simplifies
 "               the definition of mappings that insert pairs of caracters when
@@ -21,6 +21,8 @@ let s:version = 300
 "               BTW, they can be activated or desactivated by pressing <F9>
 "
 " History:      {{{1
+" Version 3.0.1:
+"               * Fix issue with old vim 7.2 version, thanks to Troy Curtis Jr
 " Version 3.0.0:
 "               * More brackets manipulations
 "               * <Plug>Mappings for brackets manipulations are now always defined.
@@ -216,6 +218,11 @@ endif
 
 "# Default brackets definitions {{{2
 if ! lh#option#get('cb_no_default_brackets', 0)
+  " Older vim versions do not properly autoload `lh#ft#is_text` wrapped in
+  " `function()`.
+  if !has('patch-7.2-061') 
+    call lh#ft#version()
+  endif
   :Brackets! ( )
   :Brackets! [ ] -visual=0
   :Brackets! [ ] -insert=0 -trigger=<leader>[

commit e92709e46d8bacaa204f9ca81badca3f9d608a66

BUG: Fix regression introduced in d4b2746cecfada0c1e5ce847f253c7ac01326ad6

autoload/lh/brackets.vim
@@ -4,9 +4,9 @@
 "               <URL:http://github.com/LucHermitte>
 " License:      GPLv3 with exceptions
 "               <URL:http://github.com/LucHermitte/lh-brackets/tree/master/License.md>
-" Version:      3.0.1
+" Version:      3.0.2
 " Created:      28th Feb 2008
-" Last Update:  01st Mar 2016
+" Last Update:  04th Mar 2016
 "------------------------------------------------------------------------
 " Description:
 "               This autoload plugin defines the functions behind the command
@@ -24,6 +24,8 @@
 "
 "------------------------------------------------------------------------
 " History:
+" Version 3.0.2:
+"               * Fix regression introduced by the support of older versions
 " Version 3.0.1:
 "               * Support older versions of vim, thanks to Troy Curtis Jr
 " Version 3.0.0:
@@ -466,7 +468,7 @@ function! s:JumpOverAllClose(chars, ...) abort
   let m = matchstr(ll, '\v^(['.a:chars.']|'.lh#marker#txt('.{-}').')+')
   " echomsg ll.'##'.m.'##'
   let len_match = lh#encoding#strlen(m)
-  if lm
+  if len_match
     let del_mark = repeat("\<del>", len_match)
     let del_mark .= substitute(m, '[^'.a:chars.']', '', 'g')
   endif

mkVba/mk-lh-map-tools.vim
@@ -4,10 +4,10 @@
 "               <URL:http://github.com/LucHermitte>
 " License:      GPLv3 with exceptions
 "               <URL:http://github.com/LucHermitte/lh-brackets/tree/master/License.md>
-" Version:	3.0.0
-let s:version = '3.0.0'
+" Version:	3.0.2
+let s:version = '3.0.2'
 " Created:	06th Nov 2007
-" Last Update:  10th Dec 2015
+" Last Update:  04th Mar 2016
 "------------------------------------------------------------------------
 cd <sfile>:p:h
 try